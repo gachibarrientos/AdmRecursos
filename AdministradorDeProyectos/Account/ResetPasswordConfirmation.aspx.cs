9ed0fb4de65319e1f00c76823fc990154df40104
gachibarrientos <gabrielbarrientos31@gmail.com>
2015-11-18 11:08:09 -0300

    Creacion de la App Web

---
+++ AdministradorDeProyectos/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace AdministradorDeProyectos.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ AdministradorDeProyectos/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdministradorDeProyectos.Startup))]
+namespace AdministradorDeProyectos
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
